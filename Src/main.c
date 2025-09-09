6021314e2ed93119238057a5c6d6234e6b892969
Kirill McQuillin <mcquillinkirill@gmail.com>
2025-09-09 14:38:27 -0400

    Remove redundant variables

--- Src/main.c
+++ Src/main.c
@@ -5,13 +5,9 @@
 uint32_t task1Stack[STACK_SIZE];
 uint32_t task2Stack[STACK_SIZE];
 TCB task1TCB;
-TCB *task1TCBPtr = &task1TCB;
 TCB task2TCB;
-TCB *task2TCBPtr = &task2TCB;
 TaskNode task1Node;
-TaskNode *task1NodePtr = &task1Node;
 TaskNode task2Node;
-TaskNode *task2NodePtr = &task2Node;
 
 // Blinking LED tasks
 void task1_blueLED()
@@ -36,8 +32,8 @@ int main(void)
 {
 	configureAll();
 
-	createTask(task1Stack, &task1_blueLED, 1, task1TCBPtr, task1NodePtr);
-	createTask(task2Stack, &task2_greenLED, 1, task2TCBPtr, task2NodePtr);
+	createTask(task1Stack, &task1_blueLED, 1, &task1TCB, &task1Node);
+	createTask(task2Stack, &task2_greenLED, 1, &task2TCB, &task2Node);
 
 	startScheduler();
 	while (1)
