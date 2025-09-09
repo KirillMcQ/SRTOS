9ce7ac86cb4bbb1f8c1e4459cf234709044ec812
Kirill McQuillin <mcquillinkirill@gmail.com>
2025-09-09 11:45:49 -0400

    Allocate static memory for task structs

--- Src/main.c
+++ Src/main.c
@@ -4,10 +4,14 @@
 // Static allocation, user must provide these memory buffers
 uint32_t task1Stack[STACK_SIZE];
 uint32_t task2Stack[STACK_SIZE];
-TCB *task1TCB;
-TCB *task2TCB;
-TaskNode *task1Node;
-TaskNode *task2Node;
+TCB task1TCB;
+TCB *task1TCBPtr = &task1TCB;
+TCB task2TCB;
+TCB *task2TCBPtr = &task2TCB;
+TaskNode task1Node;
+TaskNode *task1NodePtr = &task1Node;
+TaskNode task2Node;
+TaskNode *task2NodePtr = &task2Node;
 
 // Blinking LED tasks
 void task1_blueLED()
@@ -32,8 +36,8 @@ int main(void)
 {
 	configureAll();
 
-	createTask(task1Stack, &task1_blueLED, 1, task1TCB, task1Node);
-	createTask(task2Stack, &task2_greenLED, 1, task2TCB, task2Node);
+	createTask(task1Stack, &task1_blueLED, 1, task1TCBPtr, task1NodePtr);
+	createTask(task2Stack, &task2_greenLED, 1, task2TCBPtr, task2NodePtr);
 
 	startScheduler();
 	while (1)
